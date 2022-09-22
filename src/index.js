2fb01fec1384975dd3433bd952ad105017941f13
Maryshchyn <maryshchyn89@gmail.com>
2022-09-22 11:23:59 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+import App from './AppProfile';
+import ReactDom  from 'react-dom';
+
+
+
+
+
+
+ReactDom.render(<App />, document.querySelector('#root'))
